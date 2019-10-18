eedf565e95ae6291c140e5ebdd2054290e52503c
Matt Lagrotte <matt@lagrotte.net>
2019-10-18 11:09:05 -0400

    Update readme

--- README.md
+++ README.md
@@ -1,16 +1,14 @@
-It's me, Tierney!
+It's me, Matt!
 
 # Usage
 
 ## npm
 ```
-npx bitandbang
+npx unclvito
 ```
 
 ## GitHub Package Registry
 Assuming you have the GitHub registry set up, you can use via npx:
 ```
-npx @bnb/card
+npx @unclvito/card
 ```
-
-Not including how to use it globally because I'm not sure why you'd want this as a global command. That'd be creepy.
--- build.js
+++ build.js
@@ -15,23 +15,23 @@ const options = {
 
 // Text + chalk definitions
 const data = {
-  name: chalk.white('               Tierney Cyren'),
-  handle: chalk.white('bitandbang'),
-  work: chalk.white('Senior Cloud Developer Advocate at Microsoft'),
-  opensource: chalk.white('Node.js Community Committee ') + chalk.green('⬢'),
-  twitter: chalk.gray('https://twitter.com/') + chalk.cyan('bitandbang'),
-  npm: chalk.gray('https://npmjs.com/') + chalk.red('~bnb'),
-  github: chalk.gray('https://github.com/') + chalk.green('bnb'),
-  linkedin: chalk.gray('https://linkedin.com/in/') + chalk.blue('bitandbang'),
-  web: chalk.cyan('https://bnb.im'),
-  npx: chalk.red('npx') + ' ' + chalk.white('@bnb/card  (via GitHub Package Registry)'),
+  name: chalk.white('               Matt Lagrotte'),
+  handle: chalk.white('unclvito'),
+  work: chalk.white('Principle Member of Technical Staff at Salesforce'),
+  twitter: chalk.gray('https://twitter.com/') + chalk.cyan('unclvito'),
+  github: chalk.gray('https://github.com/') + chalk.green('unclvito'),
+  linkedin: chalk.gray('https://linkedin.com/in/') + chalk.blue('lagrotte'),
+  tesla: chalk.gray('https://ts.la/') + chalk.yellow('matt17582'),
+  npm: chalk.gray('https://npmjs.com/') + chalk.red('~unclvito'),
+  web: chalk.cyan('https://stuffbymatt.com'),
+  npx: chalk.red('npx') + ' ' + chalk.white('@unclvito/card  (via GitHub Package Registry)'),
   labelWork: chalk.white.bold('       Work:'),
-  labelOpenSource: chalk.white.bold('Open Source:'),
   labelTwitter: chalk.white.bold('    Twitter:'),
-  labelnpm: chalk.white.bold('        npm:'),
   labelGitHub: chalk.white.bold('     GitHub:'),
+  labelnpm: chalk.white.bold('        npm:'),
   labelLinkedIn: chalk.white.bold('   LinkedIn:'),
   labelWeb: chalk.white.bold('        Web:'),
+  labelTesla: chalk.white.bold('      Tesla:'),
   labelCard: chalk.white.bold('       Card:')
 }
 
@@ -39,23 +39,23 @@ const data = {
 const newline = '\n'
 const heading = `${data.name} / ${data.handle}`
 const working = `${data.labelWork}  ${data.work}`
-const opensourcing = `${data.labelOpenSource}  ${data.opensource}`
 const twittering = `${data.labelTwitter}  ${data.twitter}`
 const npming = `${data.labelnpm}  ${data.npm}`
 const githubing = `${data.labelGitHub}  ${data.github}`
 const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
 const webing = `${data.labelWeb}  ${data.web}`
 const carding = `${data.labelCard}  ${data.npx}`
+const teslaing = `${data.labelTesla}  ${data.tesla}`
 
 // Put all our output together into a single variable so we can use boxen effectively
 const output = heading + // data.name + data.handle
                newline + newline + // Add one whole blank line
                working + newline + // data.labelWork + data.work
-               opensourcing + newline + newline + // data.labelOpenSource + data.opensource
                twittering + newline + // data.labelTwitter + data.twitter
                npming + newline + // data.labelnpm + data.npm
                githubing + newline + // data.labelGitHub + data.github
                linkedining + newline + // data.labelLinkedIn + data.linkedin
+               teslaing + newline + // data.labelWeb + data.web
                webing + newline + newline + // data.labelWeb + data.web
                carding // data.labelCard + data.npx
 
